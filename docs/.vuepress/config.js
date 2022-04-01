0b5e0486cabdb66eebc30c7625cfa3f7338add30
刘怡帆 <liuyifan@shiqiao.com>
2022-04-01 14:38:10 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: 'Eric的知识星球',
+  description: 'Eric的知识星球',
+  themeConfig: {
+    nav: [
+      { text: '🙋‍♂️ 欢迎👏🏻扩列', link: '/' },
+      {
+        text: '点亮⭐不迷路',
+        items: [
+          { text: 'Github', link: '' },
+          {
+            text: '掘金',
+            link: '',
+          },
+        ],
+      },
+    ],
+  },
+}
